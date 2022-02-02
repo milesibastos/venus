073caab2c49d53ddadcf44165d9dd4abbec2a2ee
milesibastos <antonio@milesibastos.com>
2022-02-02 20:35:50 +0000

    Deploying to gh-pages from @ milesibastos/venus@49f926f8fcabfea8bbc797e1cda039ace3ff3344 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "venus/precache-manifest.d255806b3c9fec839f801c2f8460949b.js"
+  "/precache-manifest.58af95db653fcca4b049750c68895f6a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("venus/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
